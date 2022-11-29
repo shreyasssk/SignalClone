b80b31ccd1bb378e52b37863e63e468525a3f0da
Shreyas Shivakumar <pauliteshreyas@gmail.com>
2022-11-30 00:09:11 +0530

    cognito lamba trigger

--- App.tsx
+++ App.tsx
@@ -1,22 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react-native';
 
+import config from './src/aws-exports';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+Amplify.configure(config);
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+function App() {
+	const isLoadingComplete = useCachedResources();
+	const colorScheme = useColorScheme();
+
+	if (!isLoadingComplete) {
+		return null;
+	} else {
+		return (
+			<SafeAreaProvider>
+				<Navigation colorScheme={colorScheme} />
+				<StatusBar />
+			</SafeAreaProvider>
+		);
+	}
 }
+
+export default withAuthenticator(App);
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,12 +1,17 @@
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Pressable, Text } from 'react-native';
 import ChatRoomItem from '../components/ChatRoomItem';
 
 import chartRoomsData from '../assets/dummy-data/ChatRooms';
+import { Auth } from 'aws-amplify';
 
 const chatRoom1 = chartRoomsData[0];
 const chatRoom2 = chartRoomsData[1];
 
 export default function TabOneScreen() {
+	// function onLogOut() {
+	// 	Auth.signOut();
+	// }
+
 	return (
 		<View style={styles.page}>
 			<FlatList
@@ -14,6 +19,10 @@ export default function TabOneScreen() {
 				renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
 				showsVerticalScrollIndicator={false}
 			/>
+
+			{/* <Pressable onPress={onLogOut} style={styles.logout}>
+				<Text>Logout</Text>
+			</Pressable> */}
 		</View>
 	);
 }
@@ -22,6 +31,14 @@ const styles = StyleSheet.create({
 	page: {
 		backgroundColor: 'white',
 		flex: 1,
+	},
+	logout: {
+		backgroundColor: 'red',
+		height: 50,
+		margin: 10,
+		borderRadius: 50,
+		alignItems: 'center',
+		justifyContent: 'center',
 	},
 });
 
